3d20f1e0614dccb9a68b333305c0f3d7fb6a0085
OlgaBilynska <olgabilinskaya@gmail.com>
2023-12-01 12:14:53 +0200

    git deploy

---
+++ src/redux/carsOperations.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getCars = createAsyncThunk(
+    'cars/getCars',
+    async ({ }, thunkAPI) => {
+        try {
+            const queryParams = [];
+        }
+    }
+)
